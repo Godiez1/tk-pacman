86df2c65ac55fd9210d85e645fdfd6843a5bd607
Chanunya Ocharos <chanunya.o@ku.th>
2021-03-18 14:53:11 +0700

    Resolve #3

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from gamelib import Sprite, GameApp, Text
 from dir_consts import *
 from maze import Maze
 
+import random
+
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
 
@@ -24,20 +26,23 @@ class Pacman(Sprite):
         x, y = maze.piece_center(r,c)
         super().__init__(app, 'images/pacman.png', x, y)
 
+        self.state = NormalPacmanState(self)
+
     def update(self):
         if self.maze.is_at_center(self.x, self.y):
             r, c = self.maze.xy_to_rc(self.x, self.y)
 
             if self.maze.has_dot_at(r, c):
                 self.maze.eat_dot_at(r, c)
-            
+
+                self.state.random_upgrade()
+
             if self.maze.is_movable_direction(r, c, self.next_direction):
                 self.direction = self.next_direction
             else:
                 self.direction = DIR_STILL
 
-        self.x += PACMAN_SPEED * DIR_OFFSET[self.direction][0]
-        self.y += PACMAN_SPEED * DIR_OFFSET[self.direction][1]
+        self.state.move_pacman()
 
     def set_next_direction(self, direction):
         self.next_direction = direction
@@ -85,6 +90,35 @@ class PacmanGame(GameApp):
         def f():
             pacman.set_next_direction(next_direction)
         return f
+
+
+class NormalPacmanState:
+    def __init__(self, pacman):
+        self.pacman = pacman
+
+    def random_upgrade(self):
+        if random.random() < 0.1:
+            self.pacman.state = SuperPacmanState(self.pacman)
+
+    def move_pacman(self):
+        self.pacman.x += PACMAN_SPEED * DIR_OFFSET[self.pacman.direction][0]
+        self.pacman.y += PACMAN_SPEED * DIR_OFFSET[self.pacman.direction][1]
+
+
+class SuperPacmanState:
+    def __init__(self, pacman):
+        self.pacman = pacman
+        self.counter = 0
+
+    def random_upgrade(self):
+        if self.counter >= 50:
+            self.pacman.state = NormalPacmanState(self.pacman)
+        self.counter += 1
+
+    def move_pacman(self):
+        self.pacman.x += 2 * PACMAN_SPEED * DIR_OFFSET[self.pacman.direction][0]
+        self.pacman.y += 2 * PACMAN_SPEED * DIR_OFFSET[self.pacman.direction][1]
+
 
 if __name__ == "__main__":
     root = tk.Tk()
